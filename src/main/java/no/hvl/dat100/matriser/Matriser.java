de70dab24ca717940018e57ce820b22089d09d89
Duy <duyhoang@hotmail.no>
2025-10-03 10:50:44 +0200

    Update Matriser.java

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -44,33 +44,63 @@ public class Matriser {
 
 	// d)
 	public static boolean erLik(int[][] a, int[][] b) {
-        boolean status = false;
-        if (a.length == b.length){
-            for (int i = 0; i < a.length; i++) {
-                for (int j = 0; j < a[i].length; j++) {
-                    if (a[i][j] == b[i][j]){
-                        status = true;
-                    }
+        if (a.length != b.length){
+            return false;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i].length != b[i].length){
+                return false;
+            }
+            for (int j = 0; j < a[i].length; j++) {
+                if (a[i][j] != b[i][j]) {
+                    return false;
                 }
             }
         }
-        return status;
-	}
+
+        return true;
+    }
 	
 	// e)
 	public static int[][] speile(int[][] matrise) {
+        int[][] nyMatrise = new int[matrise.length][];
 
-		// TODO
+//        for (int i = 0; i < nyMatrise.length; i++) {
+//            nyMatrise[i] = new int[matrise[i].length];
+//            int k = 0;
+//
+//            for (int j = nyMatrise[i].length - 1; j >= 0 ; j--) {
+//                nyMatrise[i][k] = matrise[i][j];
+//                k++;
+//            }
+//        }
 
-		throw new UnsupportedOperationException("Metoden speile ikke implementert");
-	
+        for (int i = 0; i < nyMatrise.length; i++) {
+            nyMatrise[i] = new int[matrise[i].length];
+            for (int j = 0; j < nyMatrise[i].length; j++) {
+                nyMatrise[i][j] = matrise[j][i];
+
+            }
+
+        }
+        return nyMatrise;
 	}
 
 	// f)
 	public static int[][] multipliser(int[][] a, int[][] b) {
+        if (a[0].length == b.length){
+            int[][] nyMatrise = new int[a.length][b[0].length];
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < b[0].length; j++) {
+                    int sum = 0;
+                    for (int k = 0; k < ; k++) {
+                        
+                    }
+                }
+            }
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden multipliser ikke implementert");
-	
+        }
+
 	}
 }
